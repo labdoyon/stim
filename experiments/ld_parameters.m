90b7f526abc9248d1fdcd3b375661f3c54f82db7
Thibault Vlieghe <Thibault.Vlieghe@mcgill.ca>
2022-07-13 20:17:54 -0400

    default hand is now unset and default language is english (french may eventually be removed entirely as it won't be used during this experiment)

--- experiments/ld_parameters.m
+++ experiments/ld_parameters.m
@@ -8,7 +8,7 @@ currentOS = lower(system_dependent('getos'));
 param = struct(...
     'rawDir',           HOME, ... 
     'outputDir',        [HOME, 'output', filesep], ...      % output directory to save data (onset and .mat)
-    'LeftOrRightHand',  1,...                   % 1 = Left Hand, 2 = Right Hand
+    'LeftOrRightHand',        'unset',...                   % 1 = Left Hand, 2 = Right Hand
     'seqA',             [2 4 1 3 4 2 3 1], ...        % sequence A to execute
     'seqB',             [2 1 4 3 2 3 4 1], ...        % sequence B to execute
     'nbBlocks',         2, ...                  % number of blocs during task
@@ -17,7 +17,7 @@ param = struct(...
     'nbKeys',           20, ...                 % number of keys during task
     'IntroNbSeq',       1, ...                  % nb of sequences for pre-training    
     'durRest',          20,...                  % Duration of the Rest period
-    'language',         1, ...                  % 1 = french (default); 2 = english
+    'language',         2, ...                  % 1 = french (default); 2 = english
     'fullscreen',       0, ...                  % 0: subwindow, 1: whole desktop => see createWindow.m for modifications
     'numMonitor',       0, ...                  % 0: 1 monitor, 1: two monitors
     'flipMonitor',      0, ...                  % 0: don't flip, 1: flip monitor
