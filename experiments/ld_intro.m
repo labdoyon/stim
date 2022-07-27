138df0116d890e48fa65d8d0d6da293d8b8b5fd1
Thibault Vlieghe <Thibault.Vlieghe@mcgill.ca>
2022-07-27 19:53:12 -0400

    added intro (still missing a few features)

--- experiments/ld_intro.m
+++ experiments/ld_intro.m
@@ -19,8 +19,8 @@ function [returnCode] = ld_intro(param)
 % CREATION OF THE WINDOW
 window = createWindow(param);
 
-% Get information about the task
-l_seqUsed = param.seqA;
+% % Get information about the task
+% l_seqUsed = param.seqA;
 
 NbSeqOK = 0;
 logoriginal = [];
@@ -31,90 +31,127 @@ timeStartExperience = GetSecs;
 Screen('TextFont', window, 'Arial');
 Screen('TextSize', window, 40); 
 gold = [255, 215, 0, 255];
+black = [0, 0, 0, 255];
 
-if param.language == 1
-    DrawFormattedText(window, ...
-            'VEUILLEZ RÉALISER LENTEMENT', ...
-            'center', 100, gold);
-    DrawFormattedText(window, ...
-            'ET SANS ERREURS LA SÉQUENCE:', ...
-            'center', 200, gold);
-    DrawFormattedText(window,num2str(l_seqUsed), ...
-            'center', 300, gold);
-    DrawFormattedText(window, ...
-            '... Êtes-vous prêt a continuer? ...', ...
-            'center', 600, gold);
-    Screen('Flip', window);
-    
-elseif param.language == 2
-
-    DrawFormattedText(window,'PERFORM THE SEQUENCE SLOWLY','center',100,gold);
-    DrawFormattedText(window,'AND WITHOUT ANY ERRORS:','center',200,gold);
-    DrawFormattedText(window,num2str(l_seqUsed),'center',300,gold);
-
-    DrawFormattedText(window,'... Are you ready to continue? ...','center',600,gold);
-    Screen('Flip', window);
-end
+% Pre-experiment text
+DrawFormattedText(window,'PERFORM THE SEQUENCES SLOWLY','center',100,gold);
+DrawFormattedText(window,'AND WITHOUT ANY ERRORS:','center',200,gold);
+DrawFormattedText(window,'... Are you ready to continue? ...','center',600,gold);
+Screen('Flip', window);
 
 % Wait for TTL (or keyboard input) before starting
 % FlushEvents('keyDown');
 [~, ~, keyCode] = KbCheck(-1);
-
 strDecoded = ld_convertKeyCode(keyCode, param.keyboard);
-
 while isempty(strfind(strDecoded, '5'))
     [~, ~, keyCode] = KbCheck(-1);
     strDecoded = ld_convertKeyCode(keyCode, param.keyboard);
 end
+Screen('FillRect', window, BlackIndex(window));
 
-% Display Red cross
-% quit, keyPressed, TimePressed
-[quit, ~, ~] = displayCross(param.keyboard, window, param.durRest, ...
-                                        0, 0, 'red', 100); % 
+learning_sequence_a_or_b = [1;2];
+learning_sequence_a_or_b = learning_sequence_a_or_b(...
+    randperm(numel(learning_sequence_a_or_b)));
 
-if ~quit
-    % Testing number of good sequences entered
-    logoriginal{length(logoriginal)+1}{1} = num2str(GetSecs - timeStartExperience);
-    logoriginal{length(logoriginal)}{2} = param.task;
-    while (NbSeqOK < param.IntroNbSeq)
+% LOOP: Learning sequences
+for i = 1:numel(learning_sequence_a_or_b)
 
-        % Sequence
-        seqOK = 0;
-        index = 0;
-        keyTmp = [];
-        while seqOK == 0
-            [quit, key, timePressed] = displayCross(param.keyboard, window,0,1,0,'green',100);
+    if learning_sequence_a_or_b(i) == 1
+        l_seqUsed = param.seqA;
+        LeftOrRightHand = param.HandSoundSequenceAssociation.seqA.hand;
+    elseif learning_sequence_a_or_b(i) == 2
+        l_seqUsed = param.seqB;
+        LeftOrRightHand = param.HandSoundSequenceAssociation.seqB.hand;
+    end
+
+    % Show hand that will be used
+    if strcmp(LeftOrRightHand, 'left_hand')
+        image_hand = imread([param.rawDir 'stimuli' filesep 'left-hand_with-numbers.png']); % Left Hand
+        param.keyboard_key_to_task_element = param.left_hand_keyboard_key_to_task_element;
+    elseif strcmp(LeftOrRightHand, 'right_hand')
+        image_hand = imread([param.rawDir 'stimuli' filesep 'right-hand_with-numbers.png']); % Right Hand
+        param.keyboard_key_to_task_element = param.right_hand_keyboard_key_to_task_element;
+    end
+    texture_hand = Screen('MakeTexture', window, image_hand);
+    Screen('DrawTexture',window,texture_hand,[],[20 20 size(image_hand,2) size(image_hand,1)]);
+    Screen('Flip', window);
+    
+    pause(3)
+    Screen('FillRect', window, BlackIndex(window));
+    
+    % showing the seqquence
+    Screen('TextFont', window, 'Arial');
+    Screen('TextSize', window, 40); 
+    DrawFormattedText(window,'PERFORM THE SEQUENCE SLOWLY','center',100,gold);
+    DrawFormattedText(window,'AND WITHOUT ANY ERRORS:','center',200,gold);
+    DrawFormattedText(window,num2str(l_seqUsed),'center',300,gold);
+    Screen('Flip', window);
+    pause(8)
+    
+    % display red cross for 1 second
+    [quit, ~, ~] = displayCross(param.keyboard, window, param.shortRest, ...
+                                        0, 0, 'red', 100);
+
+    % subject must type sequence once correctly
+    if ~quit
+        % Testing number of good sequences entered
+        logoriginal{length(logoriginal)+1}{1} = num2str(GetSecs - timeStartExperience);
+        logoriginal{length(logoriginal)}{2} = param.task;
+        NbSeqOK = 0;
+        while (NbSeqOK < param.IntroNbSeq)
+    
+            % Sequence
+            seqOK = 0;
+            index = 0;
+            keyTmp = [];
+            while seqOK == 0
+                [quit, key, timePressed] = displayCross(param.keyboard, window,0,1,0,'green',100);
+                if quit 
+                    break; 
+                end
+                
+                strDecoded = ld_convertKeyCode(key, param.keyboard);
+                key = ld_convertOneKey(strDecoded);
+                
+                try
+                    key = param.keyboard_key_to_task_element(key);
+                catch ME
+                    switch ME.identifier
+                        case 'MATLAB:Containers:Map:NoKey'
+                            key = 0;
+                        otherwise
+                            ME.identifier
+                            rethrow(ME)
+                    end
+                end
+
+                disp(key)
+                
+                logoriginal{end+1}{1} = num2str(timePressed - timeStartExperience);
+                logoriginal{end}{2} = 'rep';
+                logoriginal{end}{3} = num2str(key);
+    
+                index = index + 1;
+                keyTmp(index) = key;
+                if index >= length(l_seqUsed)
+                    if keyTmp == l_seqUsed
+                        seqOK = 1;
+                        NbSeqOK = NbSeqOK + 1;
+                    else
+                        keyTmp(1) = [];
+                        index = index - 1;
+                        NbSeqOK = 0;
+                    end
+                end
+            end % End while loop: check if sequence is ok 
             if quit 
                 break; 
             end
-            
-            strDecoded = ld_convertKeyCode(key, param.keyboard);
-            key = ld_convertOneKey(strDecoded);
-
-            disp(key)
-            
-            logoriginal{end+1}{1} = num2str(timePressed - timeStartExperience);
-            logoriginal{end}{2} = 'rep';
-            logoriginal{end}{3} = num2str(key);
-
-            index = index + 1;
-            keyTmp(index) = key;
-            if index >= length(l_seqUsed)
-                if keyTmp == l_seqUsed
-                    seqOK = 1;
-                    NbSeqOK = NbSeqOK + 1;
-                else
-                    keyTmp(1) = [];
-                    index = index - 1;
-                    NbSeqOK = 0;
-                end
-            end
-        end % End while loop: check if sequence is ok 
-        if quit 
-            break; 
         end
     end
 end
+
+
 Screen('CloseAll');
 
 % Save file.mat
--- experiments/ld_parameters.m
+++ experiments/ld_parameters.m
@@ -15,8 +15,9 @@ param = struct(...
     'nbBlocksDayOne',   10, ...                 % number of blocs during task
     'nbBlocksDayTwo',   8, ...                  % number of blocs during task
     'nbKeys',           20, ...                 % number of keys during task
-    'IntroNbSeq',       1, ...                  % nb of sequences for pre-training    
+    'IntroNbSeq',       3, ...                  % nb of sequences for pre-training    
     'durRest',          20,...                  % Duration of the Rest period
+    'shortRest',        1,...                   % in seconds
     'language',         2, ...                  % 1 = french (default); 2 = english
     'fullscreen',       0, ...                  % 0: subwindow, 1: whole desktop => see createWindow.m for modifications
     'numMonitor',       0, ...                  % 0: 1 monitor, 1: two monitors
